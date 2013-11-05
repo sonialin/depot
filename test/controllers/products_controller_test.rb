dfe7e6362e505e9a2f556e4567381092a0707476
Sonia Lin <sonia_lin@hotmail.com>
2013-11-05 18:06:13 +0800

    Add customer interface and functional tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'table tr', 3
+    assert_select '#main dt', 'MyString'
   end
 
   test "should get new" do
@@ -52,4 +54,5 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
 end
