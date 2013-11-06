3c47df12d8a3ec513a9648aebe1a7b6181210bc6
Sonia Lin <sonia_lin@hotmail.com>
2013-11-06 21:57:45 +0800

    Add functionality for line item creation in cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
   def index
+  	# display products
   	@products = Product.order(:title)
+
+  	# session counter for the times the user has visited
+  	@count = increment_counter
+    @time = Time.now
   end
+
+		# session counter method
+	def increment_counter
+	  if session[:counter].nil?
+	    session[:counter] = 0
+	  end
+	  session[:counter] += 1
+	end
 end
