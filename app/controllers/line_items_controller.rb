34a40cd1e0c8bab6b1599d38cfd8f6c5856732d4
Sonia Lin <sonia_lin@hotmail.com>
2013-11-11 12:34:24 +0800

    Add increment and decreament for line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -56,6 +56,48 @@ class LineItemsController < ApplicationController
     end
   end
 
+  # PUT /line_items/1
+  # PUT /line_items/1.json
+  def decrement
+    @cart = current_cart
+
+    @line_item = @cart.line_items.find_by_id(params[:id])
+    @line_item = @line_item.decrement_quantity(@line_item.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
+        format.js {@current_item = @line_item}
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ # PUT /line_items/1
+  # PUT /line_items/1.json
+  def increment
+    @cart = current_cart
+
+    @line_item = @cart.line_items.find_by_id(params[:id])
+    @line_item = @line_item.increment_quantity(@line_item.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
+        format.js {@current_item = @line_item}
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
